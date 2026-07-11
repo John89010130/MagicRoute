5396001b4360df73170856b13f9d3fe3bb8c1fef
TID-Eric <john89010130@gmail.com>
2026-07-11 14:53:07 -0300

    fix: mudar para HashRouter para suportar F5 no GitHub Pages

--- magicroute-web/src/App.tsx
+++ magicroute-web/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider, useAuth } from './contexts/AuthContext';
 import Layout from './components/Layout';
 import Login from './pages/Login';
@@ -72,10 +72,10 @@ function AppRoutes() {
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <AuthProvider>
         <AppRoutes />
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
